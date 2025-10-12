60e1f270455bca7f2d600b61de679e6a0e103aee
reemmoha <reemmohamed0297@gmail.com>
2025-10-12 18:06:54 +0300

    Added frontend section

---
+++ Bookify.Web/Bookify.Web/Controllers/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class RoomController: Controller
+    {
+        public IActionResult Rooms()
+        {
+            return View();
+        }
+    }
+}
