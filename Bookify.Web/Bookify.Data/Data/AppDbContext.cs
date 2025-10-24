7f5f61d3701fd3cb03b1061a61902f8c9cc315ea
reemmoha <reemmohamed0297@gmail.com>
2025-10-24 21:00:12 +0300

    Update Room Seed Data, availability View and controller

--- Bookify.Web/Bookify.Data/Data/AppDbContext.cs
+++ Bookify.Web/Bookify.Data/Data/AppDbContext.cs
@@ -27,20 +27,20 @@ namespace Bookify.Data.Data
             // RoomType → Room (1-to-many)
             builder.Entity<Room>()
                 .HasOne(r => r.RoomType)
-                .WithMany(rt =>rt.Rooms)
+                .WithMany(rt => rt.Rooms)
                 .HasForeignKey(r => r.RoomTypeId)
                  .OnDelete(DeleteBehavior.Cascade);
 
             // Room → Booking (1-to-many)
             builder.Entity<Booking>()
                 .HasOne(b => b.Room)
-                .WithMany(r=> r.Bookings)
+                .WithMany(r => r.Bookings)
                 .HasForeignKey(b => b.RoomId)
                 .OnDelete(DeleteBehavior.Cascade);
 
             // User (AspNetUsers) → Booking (1-to-many)
             builder.Entity<Booking>()
-                   .HasOne<Microsoft.AspNetCore.Identity.IdentityUser>() 
+                   .HasOne<Microsoft.AspNetCore.Identity.IdentityUser>()
                    .WithMany()
                    .HasForeignKey(b => b.UserId)
                    .OnDelete(DeleteBehavior.Restrict);
@@ -51,7 +51,7 @@ namespace Bookify.Data.Data
            .HasOne(p => p.Booking)
            .WithOne(b => b.Payment)
            .HasForeignKey<Payment>(p => p.BookingId)
-           .OnDelete(DeleteBehavior.Cascade); 
+           .OnDelete(DeleteBehavior.Cascade);
 
 
             // Store enums as strings
@@ -72,12 +72,22 @@ namespace Bookify.Data.Data
                 .Property(rt => rt.PricePerNight)
                 .HasPrecision(18, 2);
 
-
-
-
-
+            builder.Entity<RoomType>().HasData(
+      new RoomType { Id = 1, Name = "Single Room", Description = "A cozy room for one guest.", PricePerNight = 800 },
+      new RoomType { Id = 2, Name = "Double Room", Description = "A comfortable room for two guests.", PricePerNight = 1200 },
+      new RoomType { Id = 3, Name = "Suite", Description = "A luxurious suite with living area.", PricePerNight = 2000 }
+  );
+            builder.Entity<Room>().HasData(
+     new Room { Id = 1, RoomNumber = "101", RoomTypeId = 1, IsAvailable = true },
+     new Room { Id = 2, RoomNumber = "102", RoomTypeId = 1, IsAvailable = true },
+     new Room { Id = 3, RoomNumber = "201", RoomTypeId = 2, IsAvailable = true },
+     new Room { Id = 4, RoomNumber = "202", RoomTypeId = 2, IsAvailable = false }, 
+     new Room { Id = 5, RoomNumber = "301", RoomTypeId = 3, IsAvailable = true },
+     new Room { Id = 6, RoomNumber = "302", RoomTypeId = 3, IsAvailable = false }  
+ );
         }
 
-    }
 
+    }
 }
+
